892db1c7ed19a2d491185d7e0baffb4520fa48fc
Sepe_4bld@student.itstep.org <Sepe_4bld@student.itstep.org>
2021-05-05 22:58:30 +0300

    DataContext

--- Calculator_Binding/View/MainWindow.xaml.cs
+++ Calculator_Binding/View/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Calculator_Binding.ViewModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Calculator_Binding
 {
@@ -20,9 +10,12 @@ namespace Calculator_Binding
     /// </summary>
     public partial class MainWindow : Window
     {
+        //ВьюМодель, содержащая в себе наблюдаемые поля
+        private CalcViewModel ViewModel;
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this.ViewModel = new CalcViewModel();
         }
     }
 }
