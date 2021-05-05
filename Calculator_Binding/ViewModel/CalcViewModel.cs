9fb96d0b00b25cd316ea96fcb6e755160e29e369
Sepe_4bld@student.itstep.org <Sepe_4bld@student.itstep.org>
2021-05-05 22:56:01 +0300

    ViewModel

---
+++ Calculator_Binding/ViewModel/CalcViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Calculator_Binding.ViewModel
+{
+    public class CalcViewModel : INotifyPropertyChanged
+    {
+        public CalcViewModel() { }
+
+        private string History = string.Empty;
+        public string HistoryProperty
+        {
+            get { return History; }
+            set { History = value; OnPropertyChanged("HistoryProperty"); }
+        }
+
+        private string Result = string.Empty;
+        public string ResultProperty
+        {
+            get { return Result; }
+            set { Result = value; OnPropertyChanged("ResultProperty"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+    }
+}
